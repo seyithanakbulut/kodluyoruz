0ba5f6e8da4edb811fd5f7af0d37b12b4ca809b2
SEYIT AKBULUT <seyithan.akbulut16@ogr.atauni.edu.tr>
2021-08-24 13:28:26 +0300

    asdasd

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        // ilk ders 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminiz:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyisminiz:");
+            string surname=Console.ReadLine();
+
+            Console.WriteLine( surname);
+
+        }
+    }
+}
